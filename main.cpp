1a479dd8141f8ec4448da68703a9d49b33492fc5
Hey913 <hey841087@gmail.com>
2025-02-19 14:37:36 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,10 @@ using namespace std;
 
 int main() {
  for (size_t i = 1; i <= 10; i++) {
-   cout << "num is " << i << endl;d
+   cout << "num is " << i << endl;
+ }
+ for (int i = 4; i < 10; i++) {
+  i -= 1;
  }
 
   return 0;
